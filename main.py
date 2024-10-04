7cac6ef1798bf5e6d2a0205888395f13c7ba893f
ktkv419 <kutikovpasha@gmail.com>
2024-10-04 23:02:10 +0600

    style: format main.py

--- main.py
+++ main.py
@@ -4,36 +4,40 @@ from datetime import datetime
 import os, glob, sys
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
-html_files = glob.glob(os.path.join(current_directory, '*.html'))
+html_files = glob.glob(os.path.join(current_directory, "*.html"))
 
 leaderboard_data = []
 
 for html_file in html_files:
     f = open(html_file, "r")
 
-    soup = BeautifulSoup(f.read(), 'html.parser')
+    soup = BeautifulSoup(f.read(), "html.parser")
 
-    table = soup.find('table', id='leaderBoard__TablePlayers')
-    if not isinstance(table, Tag): break
+    table = soup.find("table", id="leaderBoard__TablePlayers")
+    if not isinstance(table, Tag):
+        break
 
     tbody = table.find("tbody")
-    if not isinstance(tbody, Tag): break
+    if not isinstance(tbody, Tag):
+        break
 
     rows = tbody.find_all("tr")
     for tr in rows:
-        pos = tr.find('td', class_='leaderBoard__Table__Position').text.strip()
-        name = tr.find('td', class_='leaderBoard__Table__Name').find("span").text.strip()
-        points = tr.find('div', class_='leaderBoard__Table__Score__Points').find("div").text.strip()
+        pos = tr.find("td", class_="leaderBoard__Table__Position").text.strip()
+        name = (
+            tr.find("td", class_="leaderBoard__Table__Name").find("span").text.strip()
+        )
+        points = (
+            tr.find("div", class_="leaderBoard__Table__Score__Points")
+            .find("div")
+            .text.strip()
+        )
 
-        leaderboard_data.append({
-            'pos': pos,
-            'name': name,
-            'points': points
-        })
+        leaderboard_data.append({"pos": pos, "name": name, "points": points})
 
     f.close()
 
-sorted_data = sorted(leaderboard_data, key=lambda x: x['name'].lower())
+sorted_data = sorted(leaderboard_data, key=lambda x: x["name"].lower())
 
 wb = Workbook()
 ws = wb.active
@@ -44,10 +48,10 @@ if ws is None:
 ws.title = "Leaderboard"
 ws.append(["Позиция", "Имя", "Очки"])
 for entry in sorted_data:
-    ws.append([entry['pos'], entry['name'], entry['points']])
+    ws.append([entry["pos"], entry["name"], entry["points"]])
 
 timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-excel_file_path =f"leaderboard_{timestamp}.xlsx" 
+excel_file_path = f"leaderboard_{timestamp}.xlsx"
 wb.save(excel_file_path)
 
 print(f"Файл {excel_file_path} успешно сохранен.")
